212d64a59bb72fff81caa371d8007d6cb42f3e0d
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-09-03 20:44:53 +0530

    Simplify the program

---
+++ Lab-3/dijkstra-adjacencyList.c
@@ -0,0 +1,182 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int vertex, weight;
+  struct node *next;
+};
+
+struct vertex {
+  int vertex, dist;
+};
+
+struct node *addEdge(struct node *head, int vertex, int weight) {
+  struct node *newNode = (struct node *)calloc(1, sizeof(struct node));
+
+  newNode->vertex = vertex;
+  newNode->weight = weight;
+  newNode->next = head;
+
+  return newNode;
+}
+
+void heapify(struct vertex minHeap[], int size, int i, int position[]) {
+  struct vertex temp;
+
+  while ((2 * i) <= size) {
+    if ((2 * i) + 1 > size) {
+      if (minHeap[i].dist > minHeap[2 * i].dist) {
+        position[minHeap[i].vertex] = 2 * i;
+        position[minHeap[2 * i].vertex] = i;
+
+        temp = minHeap[i];
+        minHeap[i] = minHeap[2 * i];
+        minHeap[2 * i] = temp;
+      }
+
+      break;
+    }
+
+    if (minHeap[i].dist > minHeap[2 * i].dist ||
+        minHeap[i].dist > minHeap[2 * i + 1].dist) {
+      if (minHeap[2 * i].dist <= minHeap[(2 * i) + 1].dist) {
+        position[minHeap[i].vertex] = 2 * i;
+        position[minHeap[2 * i].vertex] = i;
+
+        temp = minHeap[2 * i];
+        minHeap[2 * i] = minHeap[i];
+        minHeap[i] = temp;
+
+        i = 2 * i;
+      } else if (minHeap[2 * i].dist > minHeap[(2 * i) + 1].dist) {
+        position[minHeap[i].vertex] = 2 * i + 1;
+        position[minHeap[2 * i + 1].vertex] = i;
+
+        temp = minHeap[(2 * i) + 1];
+        minHeap[(2 * i) + 1] = minHeap[i];
+        minHeap[i] = temp;
+
+        i = (2 * i) + 1;
+      }
+    } else {
+      break;
+    }
+  }
+}
+
+void buildHeap(struct vertex minHeap[], int size, int position[]) {
+  int i;
+
+  for (i = size / 2; i >= 1; --i) {
+    heapify(minHeap, size, i, position);
+  }
+}
+
+void decreaseKey(struct vertex minHeap[], struct vertex newNode,
+                 int position[]) {
+  minHeap[position[newNode.vertex]].dist = newNode.dist;
+
+  int i = position[newNode.vertex];
+  struct vertex temp;
+
+  while (i > 1) {
+    if (minHeap[i / 2].dist > minHeap[i].dist) {
+      position[minHeap[i].vertex] = i / 2;
+      position[minHeap[i / 2].vertex] = i;
+
+      temp = minHeap[i / 2];
+      minHeap[i / 2] = minHeap[i];
+      minHeap[i] = temp;
+
+      i = i / 2;
+    } else {
+      break;
+    }
+  }
+}
+
+struct vertex extractMin(struct vertex minHeap[], int size, int position[]) {
+  position[minHeap[1].vertex] = size;
+  position[minHeap[size].vertex] = 1;
+
+  struct vertex min = minHeap[1];
+
+  minHeap[1] = minHeap[size];
+  --size;
+  heapify(minHeap, size, 1, position);
+
+  return min;
+}
+
+void dijkstra(struct node *adjacencyList[], int vertices, int startVertex,
+              int distances[]) {
+  int i;
+  struct vertex minVertex;
+  struct vertex priorityQueue[vertices + 1];
+  int position[vertices + 1];
+
+  for (i = 1; i <= vertices; ++i) {
+    distances[i] = INT_MAX;
+    priorityQueue[i].dist = INT_MAX;
+    priorityQueue[i].vertex = i;
+    position[i] = priorityQueue[i].vertex;
+  }
+
+  distances[startVertex] = 0;
+  priorityQueue[startVertex].dist = 0;
+  buildHeap(priorityQueue, vertices, position);
+
+  for (i = 1; i <= vertices; ++i) {
+    minVertex = extractMin(priorityQueue, vertices, position);
+    struct node *adjacent = adjacencyList[minVertex.vertex];
+
+    while (adjacent != NULL) {
+      int u = minVertex.vertex;
+      int v = adjacent->vertex;
+      int w = adjacent->weight;
+
+      if (distances[u] != INT_MAX && distances[v] > distances[u] + w) {
+        distances[v] = distances[u] + w;
+        struct vertex changedVertex;
+
+        changedVertex.vertex = v;
+        changedVertex.dist = distances[v];
+        decreaseKey(priorityQueue, changedVertex, position);
+      }
+
+      adjacent = adjacent->next;
+    }
+  }
+}
+
+int main() {
+  int vertices, edges, i, v1, v2, w, startVertex;
+
+  printf("Enter the Number of Vertices: ");
+  scanf("%d", &vertices);
+  printf("Enter the Number of Edges: ");
+  scanf("%d", &edges);
+
+  struct node *adjacencyList[vertices + 1];
+  int distances[vertices + 1];
+
+  for (i = 0; i <= vertices; ++i) {
+    adjacencyList[i] = NULL;
+  }
+
+  for (i = 1; i <= edges; ++i) {
+    scanf("%d%d%d", &v1, &v2, &w);
+    adjacencyList[v1] = addEdge(adjacencyList[v1], v2, w);
+  }
+
+  printf("Enter start vertex -\n");
+  scanf("%d", &startVertex);
+  dijkstra(adjacencyList, vertices, startVertex, distances);
+  printf("Shortest distances -\n");
+
+  for (i = 1; i <= vertices; ++i) {
+    printf("Vertex %d, Distance = %d\n", i, distances[i]);
+  }
+  return 0;
+}
