3b6c67e2cbcb9985ae2c8e05d32edc384cfdc1f2
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-09-04 22:31:39 +0530

    Give new input sizes

--- Lab-3/adjacencyList-generator.c
+++ Lab-3/adjacencyList-generator.c
@@ -5,9 +5,9 @@
 int main() {
   FILE *p;
   p = fopen("in-list.txt", "w");
-  int sizes[3] = {10, 100, 1000};
-  fprintf(p, "3\n");
-  for (size_t i = 0; i < 3; i++) {
+  int sizes[4] = {10, 100, 1000, 10000};
+  fprintf(p, "4\n");
+  for (size_t i = 0; i < 4; i++) {
     srand(i);
     fprintf(p, "%d\n", sizes[i]);
     int edges = rand() % (sizes[i] * sizes[i]);
