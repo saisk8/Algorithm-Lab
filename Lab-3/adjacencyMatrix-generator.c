044bcee01683d68b194b147d13d33a515baf39db
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-09-05 00:20:52 +0530

    Reduce input size

--- Lab-3/adjacencyMatrix-generator.c
+++ Lab-3/adjacencyMatrix-generator.c
@@ -3,20 +3,20 @@
 #define WEIGHT_LIMIT 200
 
 int main() {
-  FILE *p;
-  p = fopen("in-matrix.txt", "w");
-  int sizes[4] = {10, 100, 1000, 10000};
-  fprintf(p, "4\n");
-  for (size_t i = 0; i < 4; i++) {
-    srand(i);
-    fprintf(p, "%d\n", sizes[i]);
-    for (int j = 0; j < sizes[i]; j++) {
-      for (int k = 0; k < sizes[i]; k++)
-        fprintf(p, "%d ", rand() % WEIGHT_LIMIT);
-      fprintf(p, "\n");
+    FILE *p;
+    p = fopen("in-matrix.txt", "w");
+    int sizes[3] = {10, 100, 1000};
+    fprintf(p, "3\n");
+    for (size_t i = 0; i < 4; i++) {
+        srand(i);
+        fprintf(p, "%d\n", sizes[i]);
+        for (int j = 0; j < sizes[i]; j++) {
+            for (int k = 0; k < sizes[i]; k++)
+                fprintf(p, "%d ", rand() % WEIGHT_LIMIT);
+            fprintf(p, "\n");
+        }
+        fprintf(p, "%d\n", (rand() % sizes[i]));
     }
-    fprintf(p, "%d\n", (rand() % sizes[i]));
-  }
-  fclose(p);
-  return 0;
+    fclose(p);
+    return 0;
 }
---
+++ Lab-3/readme.txt
@@ -0,0 +1,11 @@
+For adjacency list and min heap algorithm: 
+N Value      Time Taken     
+10        : 0.000003       
+100       : 0.000101       
+1000      : 0.012918       
+10000     : 0.428425       
+For adjacency matrix and min heap algorithm: 
+N Value      Time Taken     
+10        : 0.000004       
+100       : 0.000123       
+1000      : 0.011525       
