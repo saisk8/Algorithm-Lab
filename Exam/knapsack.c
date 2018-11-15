9b0326e28dee4c9ace84024ae93875ce81fe131e
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-11-15 21:42:04 +0530

    Change recursion to DP

--- Exam/knapsack.c
+++ Exam/knapsack.c
@@ -1,52 +1,34 @@
 #include <stdio.h>
 
-int w[10], p[10], v[10][10], n, i, j, cap, x[10] = {0};
+// A utility function that returns maximum of two integers
+int max(int a, int b) { return (a > b) ? a : b; }
 
-int max(int i, int j) { return ((i > j) ? i : j); }
+// Returns the maximum value that can be put in a knapsack of capacity W
+int knapSack(int W, int wt[], int val[], int n) {
+    int i, w;
+    int K[n + 1][W + 1];
 
-int knap(int i, int j) {
-    int value;
-    if (v[i][j] < 0) {
-        if (j < w[i])
-            value = knap(i - 1, j);
-        else
-            value = max(knap(i - 1, j), p[i] + knap(i - 1, j - w[i]));
-        v[i][j] = value;
+    // Build table K[][] in bottom up manner
+    for (i = 0; i <= n; i++) {
+        for (w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                K[i][w] =
+                    max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
     }
-    return (v[i][j]);
+
+    return K[n][W];
 }
 
-void main() {
-    int profit, count = 0;
-    printf("\nEnter the number of elements\n");
-    scanf("%d", &n);
-    printf("Enter the profit and weights of the elements\n");
-    for (i = 1; i <= n; i++) {
-        printf("For item no %d\n", i);
-        scanf("%d%d", &p[i], &w[i]);
-    }
-    printf("\nEnter the capacity \n");
-    scanf("%d", &cap);
-    for (i = 0; i <= n; i++)
-        for (j = 0; j <= cap; j++)
-            if ((i == 0) || (j == 0))
-                v[i][j] = 0;
-            else
-                v[i][j] = -1;
-    profit = knap(n, cap);
-    i = n;
-    j = cap;
-    while (j != 0 && i != 0) {
-        if (v[i][j] != v[i - 1][j]) {
-            x[i] = 1;
-            j = j - w[i];
-            i--;
-        } else
-            i--;
-    }
-    printf("Items included are\n");
-    printf("Sl.no\tweight\tprofit\n");
-    for (i = 1; i <= n; i++)
-        if (x[i]) printf("%d\t%d\t%d\n", ++count, w[i], p[i]);
-    printf("Total profit = %d\n", profit);
+int main() {
+    int val[] = {60, 100, 120};
+    int wt[] = {10, 20, 30};
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+    printf("%d", knapSack(W, wt, val, n));
+    return 0;
 }
